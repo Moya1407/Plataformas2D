fdc4bae5fe50893184dbaa4fab8c26dc9bd203dc
Moya1407 <Manumoya7@gmail.com>
2016-11-09 18:25:03 +0100

    Actualizacion 9/11 18:24
    Moneda con fuerza aleatoria, Gizmos en teletransporte, material goma

---
+++ Assets/Scripts/Moneda.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moneda : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 6);
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2(Random.Range(-500,500),100));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
